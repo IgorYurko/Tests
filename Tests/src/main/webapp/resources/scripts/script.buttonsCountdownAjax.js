471c5d3bfb32dfa371f901467731460f3a5ed99d
Igor Yurko <igor.yurko.mail@gmail.com>
2015-08-07 09:57:10 +0300

    Fix ajax bag in view test

--- Tests/src/main/webapp/resources/scripts/script.buttonsCountdownAjax.js
+++ Tests/src/main/webapp/resources/scripts/script.buttonsCountdownAjax.js
@@ -2,18 +2,37 @@ $(document).ready(function() {
 	
 	$("#submit").button();
 	$("#radio").buttonset();
-	$("label").addClass("ui-corner-all");	
-	
+	$("label").addClass("ui-corner-all");
+		
 	$("#timer").countdown({until: '+0h +0m +30s', format: 'S', significant: 1, compact: true});
+
 	
 	setTimeout(function(){
-		myAjaxPost({
-			url : "test",
-			contentType : "application/x-www-form-urlencoded",
-			dataName : "answerForm",
-			dataVal : "NaN",
-			html : $("body")
-		});
-	}, 30100);
+		
+		setInterval(function(){
+			
+			if($("#timer").find("span.countdown-row").text() == "00")
+				myAjaxPost({
+					url : "test",
+					contentType : "application/x-www-form-urlencoded",
+					dataName : "answerForm",
+					dataVal : "NaN",
+					html : $("body")
+				}); 
+			
+		}, 500);
+			
+	}, 29000);
+		
+//	countdown.on("change", function(){
+//		if($(this) == "0"){
+//			
+//		}
+//	});
+	
+//	setTimeout(function(){
+//		
+//		
+//	}, 30100);
 	
 });
